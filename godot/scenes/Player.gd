9617982db63f65a7e133c8b5a494f72ae2f04a24
konsumlamm <konsumlamm@gmail.com>
2023-04-29 02:04:39 +0200

    Simplify player logic and add wrapping

--- godot/scenes/Player.gd
+++ godot/scenes/Player.gd
@@ -3,20 +3,10 @@ extends CharacterBody2D
 const SPEED = 300.0
 
 func _physics_process(delta):
-	var direction
-	direction = Input.get_axis("ui_up", "ui_down")
-	if direction:
-		velocity.y = direction * SPEED
-	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
-	
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = direction * SPEED
 	move_and_slide()
+
+	var screen_size = get_viewport_rect().size
+	position.x = fposmod(position.x, screen_size.x)
+	position.y = fposmod(position.y, screen_size.y)
