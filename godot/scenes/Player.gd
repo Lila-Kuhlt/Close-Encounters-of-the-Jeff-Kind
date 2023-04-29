e41282968bf308b933f9829c59380d8293b13cc6
konsumlamm <konsumlamm@gmail.com>
2023-04-29 02:49:59 +0200

    Support WASD

--- godot/scenes/Player.gd
+++ godot/scenes/Player.gd
@@ -2,11 +2,12 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 
-func _physics_process(delta):
-	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+func _physics_process(_delta):
+	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * SPEED
 	move_and_slide()
 
+	# wrap position
 	var screen_size = get_viewport_rect().size
 	position.x = fposmod(position.x, screen_size.x)
 	position.y = fposmod(position.y, screen_size.y)
