11377eae1fdbc1e40f50fff5ff72b59c56efc1f0
EliasSchaut <eschaut@outlook.de>
2023-04-29 15:17:25 +0200

    create new bullet type (line bullet)

--- godot/src/Alien.gd
+++ godot/src/Alien.gd
@@ -1,12 +1,15 @@
 extends CharacterBody2D
-const bullet_scene = preload("res://scenes/Bullet.tscn")
+const bullet_scene = preload("res://scenes/bullets/Line.Bullet.tscn")
 var bullet
 
 var x = 0
+var angle = 0
 
 func _on_timer_timeout():
 	bullet = bullet_scene.instantiate()
 	bullet.direction = Vector2(sin(x), cos(x))
+	angle = atan2(bullet.direction.y, bullet.direction.x) - PI/2
+	bullet.rotate(angle)
 	bullet.position = position
 	x += 0.5
 	get_parent().add_child(bullet)
