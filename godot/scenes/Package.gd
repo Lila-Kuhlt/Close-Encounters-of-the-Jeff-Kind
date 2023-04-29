ee681a341c5a5b9a7d37cdac164c98e3258cd485
konsumlamm <konsumlamm@gmail.com>
2023-04-29 16:25:53 +0200

    Implement package spawning, collecting and despawning

--- godot/scenes/Package.gd
+++ godot/scenes/Package.gd
@@ -5,3 +5,10 @@ var destination: Vector2i
 ## Starts the timer with `time_sec` seconds and connects the callback to the `timeout` signal.
 func start_timer(time_sec: float, callback: Callable):
 	get_tree().create_timer(time_sec).timeout.connect(callback)
+
+func pick_up():
+	visible = false
+	$Area2D.set_deferred("monitoring", false)
+
+func _on_area_2d_body_entered(player):
+	player.collect_package(self)
