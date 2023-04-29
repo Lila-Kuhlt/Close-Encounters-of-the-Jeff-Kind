baf83665cd666d4ab593d474c69c0a0078c04dc9
konsumlamm <konsumlamm@gmail.com>
2023-04-29 13:31:53 +0200

    Move package spawn logic to `World.gd`
    Track missed packages

--- godot/scenes/Player.gd
+++ godot/scenes/Player.gd
@@ -5,45 +5,28 @@ const Bullet = preload("res://scenes/Bullet.tscn")
 
 const SPEED: float = 300.0
 const MAX_QUEUE_SIZE: int = 5
-const PACKAGE_SPAWN_AREA_ATLAS_COORDS := Vector2i(2, 2)
-
-var rng := RandomNumberGenerator.new()
 
 var package_queue := []
-var package_spawn_areas := []
-
-func populate_package_spawn_areas():
-	var ground: TileMap = get_parent().get_node('GroundTileMap')
-	var areas := ground.get_used_cells_by_id(0, 1, PACKAGE_SPAWN_AREA_ATLAS_COORDS)
-	for area in areas:
-		package_spawn_areas.append(ground.map_to_local(area))
-
-func _ready():
-	populate_package_spawn_areas()
-
-func build_package():
-	var package: Node2D = Package.instantiate()
-	var idx := rng.randi_range(0, len(package_spawn_areas))
-	package.position = package_spawn_areas[idx]
-	return package
+var missed_packages := 0
 
 ## Tries to add a package to the queue and returns wether the package could be added.
-func add_package() -> bool:
+func add_package(package) -> bool:
 	if package_queue.size() < MAX_QUEUE_SIZE:
-		var package = build_package()
-		get_parent().add_child(package)
 		package.start_timer(2.0, _on_package_timeout.bind(package))
 		package_queue.append(package)
-		print("added package")
 		return true
 	else:
 		return false
 
-## This function is called when a package timer reaches 0.
-func _on_package_timeout(package):
-	print("dropped package")
+## Removes a package from the queue and removes the node.
+func remove_package(package):
 	package_queue.erase(package)
 	package.queue_free()
+
+## This function is called when a package timer reaches 0.
+func _on_package_timeout(package):
+	remove_package(package)
+	missed_packages += 1
 
 func _physics_process(_delta):
 	var direction := Input.get_vector("left", "right", "up", "down")
