796f3b734894049c932a6b8282d7b69170c28bb6
nat-rix <upezu@student.kit.edu>
2023-04-29 15:56:21 +0200

    Rework tile maps and move buildings to object map

--- godot/scenes/World.gd
+++ godot/scenes/World.gd
@@ -3,21 +3,45 @@ extends Node2D
 const Package = preload("res://scenes/Package.tscn")
 const Ufo = preload("res://scenes/Ufo.tscn")
 
-const PACKAGE_SPAWN_AREA_ATLAS_COORDS := Vector2i(2, 2)
-
 @export var ufo_spawn_chance = 0.5
 
 var rng := RandomNumberGenerator.new()
 var package_spawn_areas := []
 
+class TileLocation:
+	var source_id: int
+	var atlas_pos: Vector2i
+	
+	func _init(source_id: int, atlas_pos: Vector2i):
+		self.source_id = source_id
+		self.atlas_pos = atlas_pos
+
+func get_spawnable_areas() -> Array[TileLocation]:
+	var ts: TileSet = $ObjectTileMap.tile_set
+	var tile_locs: Array[TileLocation] = []
+	for ix in range(ts.get_source_count()):
+		var sid := ts.get_source_id(ix)
+		var source: TileSetAtlasSource = ts.get_source(sid)
+		var gridsz: Vector2i = source.get_atlas_grid_size()
+		for y in range(gridsz.y):
+			for x in range(gridsz.x):
+				var data = source.get_tile_data(Vector2i(x, y), 0)
+				if data == null:
+					continue
+				if data.get_custom_data('canPackagesSpawn'):
+					tile_locs.append(TileLocation.new(sid, Vector2i(x, y)))
+	return tile_locs
+
 func _ready():
 	populate_package_spawn_areas()
 
 func populate_package_spawn_areas():
-	var ground: TileMap = get_node('GroundTileMap')
-	var areas := ground.get_used_cells_by_id(0, 1, PACKAGE_SPAWN_AREA_ATLAS_COORDS)
-	for area in areas:
-		package_spawn_areas.append(ground.map_to_local(area))
+	var map: TileMap = get_node('ObjectTileMap')
+	var locs := get_spawnable_areas()
+	for loc in locs:
+		var areas := map.get_used_cells_by_id(0, loc.source_id, loc.atlas_pos)
+		for area in areas:
+			package_spawn_areas.append(map.map_to_local(area))
 
 func build_package():
 	var package: Node2D = Package.instantiate()
