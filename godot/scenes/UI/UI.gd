1f625fcfe236f5df1119b8127350be1dcc96b322
nat-rix <upezu@student.kit.edu>
2023-04-30 02:07:52 +0200

    Add pause menu

--- godot/scenes/UI/UI.gd
+++ godot/scenes/UI/UI.gd
@@ -1,5 +1,19 @@
 extends CanvasLayer
 
-# Called when the node enters the scene tree for the first time.
+var just_paused := false
+
 func _ready():
 	add_to_group("UI")
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed('pause'):
+		if not just_paused:
+			get_tree().paused = true
+			$PauseMenu.show()
+	else:
+		just_paused = false
+
+func _on_button_pressed() -> void:
+	just_paused = true
+	get_tree().paused = false
+	$PauseMenu.hide()
