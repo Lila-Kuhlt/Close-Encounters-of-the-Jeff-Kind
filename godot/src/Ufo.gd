8638086ba3def4362ff00f10cbb2b608968376dd
nat-rix <upezu@student.kit.edu>
2023-04-30 07:31:10 +0200

    Make ufos bullet hellier

--- godot/src/Ufo.gd
+++ godot/src/Ufo.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 const MARGIN := Vector2(10, 10)
-const SPEED: float = 0.25
+const SPEED: float = 0.2
 
 const Bullet = preload("res://scenes/bullets/Point.Bullet.tscn")
 
@@ -53,15 +53,16 @@ func mirror_point(point: Vector2, center: Vector2) -> Vector2:
 	return 2 * center - point
 
 func _on_timer_timeout():
-	var bullet = Bullet.instantiate()
+	for alpha in [1, -1]:
+		var bullet = Bullet.instantiate()
 
-	bullet.direction = Vector2(dest.x - start.x, dest.y - start.y) * 0.005
-	var r := randf_range(-1, 1)
-	bullet.direction = bullet.direction.rotated(r * 0.05)
-	bullet.position = position
-	bullet.transform = bullet.transform.scaled_local(Vector2(0.25, 0.25))
+		var d := (dest - start) * 0.0005
+		bullet.direction = d.rotated(alpha)
+		# var r := randf_range(-1, 1)
+		# bullet.direction = bullet.direction.rotated(r * 0.05)
+		bullet.position = position
 
-	get_parent().add_child(bullet)
+		get_parent().add_child(bullet)
 
 
 
