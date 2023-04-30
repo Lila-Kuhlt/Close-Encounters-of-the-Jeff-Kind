c241c62f9cd9866beab2f75e1a8e977a5a917ef0
nat-rix <upezu@student.kit.edu>
2023-04-30 22:33:48 +0200

    Add rocket bullet animation

--- godot/scenes/bullets/HomingBullet.gd
+++ godot/scenes/bullets/HomingBullet.gd
@@ -2,7 +2,11 @@ extends "res://scenes/bullets/Bullet.gd"
 
 const HOMING_FACTOR := 0.01
 
+func _ready() -> void:
+	$AnimationPlayer.play('rocket_fly')
+
 func _physics_process(delta):
 	var player_direction = position.direction_to(get_parent().get_node("Jeff").position)
 	direction = lerp(direction, player_direction, HOMING_FACTOR)
+	rotation = direction.angle() + PI * 0.5
 	super._physics_process(delta)
