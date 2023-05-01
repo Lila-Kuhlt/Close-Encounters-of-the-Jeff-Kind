089e6641517106d64d378c25b21c30ac9aaf9ac1
nat-rix <upezu@student.kit.edu>
2023-05-02 01:23:00 +0200

    Better startup screen

--- godot/scenes/UI/TitleScreen.gd
+++ godot/scenes/UI/TitleScreen.gd
@@ -3,9 +3,12 @@ extends CanvasLayer
 func _ready():
 	if Globals.DEBUG:
 		_on_start_button_pressed()
+	get_window().content_scale_aspect = Window.CONTENT_SCALE_ASPECT_KEEP
 	$Panel/CenterContainer/VBoxContainer/StartButton.grab_focus()
+	$AnimationPlayer.play('title_rotate')
 
 func _on_start_button_pressed():
+	get_window().content_scale_aspect = Window.CONTENT_SCALE_ASPECT_EXPAND
 	get_tree().change_scene_to_file("res://scenes/World.tscn")
 
 func _on_quit_button_pressed():
