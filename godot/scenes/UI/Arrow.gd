8f554e8cc3b9ecc0b28a93b9877093062a96b687
konsumlamm <konsumlamm@gmail.com>
2023-05-02 02:30:27 +0200

    Properly fix arrow positioning

--- godot/scenes/UI/Arrow.gd
+++ godot/scenes/UI/Arrow.gd
@@ -4,52 +4,40 @@ var destination: Node2D
 var progress_bar: Node2D
 
 const DESTINATION_MARGIN := 13
-const LEFT_CLIP := 7.0
 
 func translate_to_inner(pos: Vector2, rect: Rect2) -> Vector2:
 	var c := rect.get_center()
-	var ctop := pos - c
-	if abs(ctop.y) < 0.01:
-		if pos.y <= 0:
-			return Vector2(c.x, 0)
-		else:
-			return Vector2(c.x, rect.end.y)
-	if abs(ctop.x) < 0.01:
-		if pos.x <= 0:
-			return Vector2(0, c.y)
-		else:
-			return Vector2(rect.end.x, c.y)
-	var ctopxy := c.y * ctop.x / ctop.y
-	var toprx := c.x - ctopxy
-	if toprx >= 0 and toprx <= rect.end.x:
-		if ctop.y <= 0:
-			return Vector2(toprx, 0)
-		return Vector2(c.x + ctopxy, rect.end.y)
-	var ctopyx := c.x * ctop.y / ctop.x
-	if ctop.x <= 0:
-		return Vector2(0, c.y - ctopyx)
-	return Vector2(rect.end.x, c.y + ctopyx)
+	var rect1 := rect.position
+	var rect2 := rect.position + Vector2(rect.size.x, 0)
+	var rect3 := rect.end
+	var rect4 := rect.position + Vector2(0, rect.size.y)
+
+	var x = Geometry2D.segment_intersects_segment(c, pos, rect1, rect2)
+	if x != null: return x
+	x = Geometry2D.segment_intersects_segment(c, pos, rect2, rect3)
+	if x != null: return x
+	x = Geometry2D.segment_intersects_segment(c, pos, rect3, rect4)
+	if x != null: return x
+	x = Geometry2D.segment_intersects_segment(c, pos, rect4, rect1)
+	assert(x != null)
+	return x
 
 func _process(_delta: float) -> void:
 	if not is_instance_valid(destination):
 		return
 	var rect := get_viewport().get_visible_rect()
-	var margin = progress_bar.radius_outer + 5.0
-	rect.size -= Vector2(margin, margin)
+	var margin = progress_bar.radius_outer + 2.0
+	rect.position += Vector2(margin, margin)
+	rect.size -= 2 * Vector2(margin, margin)
 	var pos: Vector2 = destination.get_canvas_transform() * (destination.position - 0.5 * $Texture.size)
 	var dir := rect.get_center().direction_to(pos)
 
 	if not rect.has_point(pos):
-		pos = translate_to_inner(pos, rect)
+		pos = translate_to_inner(pos, rect) - 0.5 * $Texture.size
 	else:
 		pos -= DESTINATION_MARGIN * dir
 
-	if pos.x < LEFT_CLIP:
-		pos.x = LEFT_CLIP
-	if pos.y < LEFT_CLIP:
-		pos.y = LEFT_CLIP
-
 	$Texture.position = pos
 	$Texture.rotation = Vector2(0, -1).angle_to(dir)
-	
+
 	progress_bar.position = pos + 0.5 * $Texture.size
